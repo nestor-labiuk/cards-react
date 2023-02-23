510598c8e3cf0bb3f3e209edcabf40dcec62d6de
nestor-labiuk <nestorlabiuk@gmail.com>
2023-02-23 20:23:58 -0300

    refactoriza button7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,19 @@
 import './header.css'
 import Navbar from '../Navbar/Navbar'
-import BurgerButon from '../Buttons/BurgerButon'
+// import BurgerButon from '../Buttons/BurgerButon'
+// import {useState} from 'react'
 
 const Header = () => {
+
+  // const [openButton, setopenButton] = useState(false)
+
+
   return (
     <header className="header">
       <h1>The online store</h1>
 
       <div className="header-navbar">
-        <div className="nav-button-close">
-          <BurgerButon
-            name='Close'
-          />
-        </div>
-        <div className="nav-button-open">
-          <BurgerButon
-            name='Open'
-          />
-        </div>
+        
         <div className="header-navbar-nav">
           <Navbar
             home='Home'
