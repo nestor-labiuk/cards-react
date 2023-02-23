6d53eef1bc925ca6d20fe19ecb50d49dd17703cd
nestor-labiuk <nestorlabiuk@gmail.com>
2023-02-23 20:24:29 -0300

    refactoriza button7

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,25 @@
 import './navbar.css'
 import BurgerButon from '../Buttons/BurgerButon'
+import {useState} from 'react'
 
 const Navbar = (props) => {
 
+  const [openButton, setopenButton] = useState(false)
+
+
+
   return (
-    <nav  className='nav'>
+    <nav className='nav'>
+
      
-      <div className="nav-button-open">
-        <BurgerButon
-          name='Open'
-        />
-      </div>
-      <div className="nav-button-close">
-        <BurgerButon
-          name='Close'
-        />
-      </div>
+      {openButton ?
+        <div className="nav-button-close" onClick={() => setopenButton(false)}>
+          <BurgerButon name='Close' /></div> :
+        <div className="nav-button-open" onClick={() => setopenButton(true)}>
+          <BurgerButon name='Open' /></div>
+      }
+
+
       <ul className='nav-list'>
         <li><a href="/">{props.home}</a></li>
         <li><a href="/">{props.aboutUs}</a></li>
