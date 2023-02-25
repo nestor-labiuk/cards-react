358a3b4f80bf2d3dea2ebf63e97f5589f3ce3fc7
nestor-labiuk <nestorlabiuk@gmail.com>
2023-02-25 17:16:22 -0300

    agrega carpeta para descripcion de productos y oara productos por caegiria

---
+++ src/pages/Description-product/Description-product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './description-product.css'
+
+
+const DescriptionProduct = () => {
+  return (
+    <div className="product">
+      <h2>This page is for product description</h2>
+    </div>
+  )
+}
+
+export default DescriptionProduct
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,11 @@
+import './products.css'
+
+const Products = () => {
+  return (
+    <div className="products">
+      <h2> This page is for Products by category</h2>      
+    </div>
+  )
+}
+
+export default Products
